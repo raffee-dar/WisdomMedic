8cea15455c3c63bca4e8633a0a6459bc315a83a1
Raffee <raffee.dar@gmail.com>
2023-08-18 16:43:14 +0530

    updates

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 $(function(){
   var aptData, displayData, sortBy, sortDir;
-
+  sortBy='aptDate';
+  sortDir='asc';
   // EVENTS
   $('#addApt').on('click', function(){
     $('.card-body').toggle(300);
@@ -14,27 +15,31 @@ $(function(){
     aptData =_.without(aptData,whichApt);
   }
 
+  function listAppointments(info){
+    info = (sortDir ==='asc') ? _.sortBy(info,sortBy):_.sortBy(info,sortBy).reverse();
+
+    $('#patList').loadTemplate('appointment-list.html',info,{
+      complete: function(){
+        $('.pet-delete').on('click',function(){
+          $(this).parents('.pet-item').hide(300, function(){
+            var whichItem = $(this).attr('id');
+            removeApt(whichItem);
+            console.log(aptData);
+            $(this).remove();
+  
+          })
+        }); //delete apt
+      }
+    });//load template  
+  }
+
 //READ DATA
 $.ajax({
   url: 'js/data.json'
 }).done(function(data) {
   aptData = data;
+  listAppointments(aptData);
   
-  $('#patList').loadTemplate('appointment-list.html',data,{
-    complete: function(){
-      $('.pet-delete').on('click',function(){
-        $(this).parents('.pet-item').hide(300, function(){
-          var whichItem = $(this).attr('id');
-          removeApt(whichItem);
-         // data.splice(Number(whichItem),1);
-          console.log(aptData);
-          $(this).remove();
-
-        })
-      }); //delete apt
-    }
-  });//load template
-
 });
 
 
